7acc6143457790569ff4d6a14e2349945be8168c
PenelopeJones <pj321@cam.ac.uk>
2021-10-21 16:37:34 +0100

    Training for fixed discharge predictions

--- shell_scripts/subm_n-step.sh
+++ shell_scripts/subm_n-step.sh
@@ -5,4 +5,4 @@ mkdir ../results/$experiment_name
 mkdir ../results/${experiment_name}/models
 mkdir ../results/${experiment_name}/predictions
 
-sbatch --time=0:10:00 -J next-cycle --export=script="../experiments/next-cycle-capacity.py",kwargs="" run-battery-cpu
+sbatch --time=0:10:00 -J n-steps --export=script="../experiments/n-step-lookahead.py",kwargs="" run-battery-cpu
--- shell_scripts/subm_train-vd.sh
+++ shell_scripts/subm_train-vd.sh
@@ -5,4 +5,4 @@ mkdir ../results/$experiment_name
 mkdir ../results/${experiment_name}/models
 mkdir ../results/${experiment_name}/predictions
 
-sbatch --time=0:10:00 -J next-cycle --export=script="../experiments/next-cycle-capacity.py",kwargs="" run-battery-cpu
+sbatch --time=0:10:00 -J vd-train --export=script="../experiments/variable-discharge-train.py",kwargs="" run-battery-cpu
